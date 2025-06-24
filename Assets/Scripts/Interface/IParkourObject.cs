e47ebe3df4d306756c7085f1cf84eb45b32ce759
hvvan <eminhwan12@gmail.com>
2025-06-24 20:08:16 +0900

    [Feat] 파쿠르 인터페이스 추가

---
+++ Assets/Scripts/Interface/IParkourObject.cs
@@ -0,0 +1,12 @@
+﻿using Player;
+using Player.Parkour;
+
+namespace Interface
+{
+    public interface IParkourObject
+    {
+        public ParkourObjectType parkourObjectType { get; }
+        public bool CanPerform();
+        public void Perform(ParkourCharacterController controller);
+    }
+}
